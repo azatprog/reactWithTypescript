9adb2525f2593a7fe4a147f26502f96a26151778
Azat Nobatov <azatprog@gmail.com>
2021-10-26 15:02:52 +0500

    add data model

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import Todos from './components/todos/Todos';
+import Todo from './models/Todo';
 
 function App() {
-    const todos = ['Learn React', 'Learn Typescript'];
+    const todos: Todo[] = [
+        new Todo('Learn React'),
+        new Todo('Learn Typescript')
+    ];
     return (
         <div className="App">
             <Todos items={todos} />
--- src/components/todos/Todos.tsx
+++ src/components/todos/Todos.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Todo from '../../models/Todo';
 
-const Todos: React.FC<{ items: string[] }> = ({ items }) => {
+const Todos: React.FC<{ items: Todo[] }> = ({ items }) => {
     return (
         <ul>
-            {items.map((item: string) => <li key={item}> {item} </li>)}
+            {items.map((item: Todo) => <li key={item.id}> {item.text} </li>)}
         </ul>
     );
 };
