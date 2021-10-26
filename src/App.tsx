012292dc3cd3bcf21ef04c447f856c70a6ae6d0c
Azat Nobatov <azatprog@gmail.com>
2021-10-26 14:52:03 +0500

    add type to props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import './App.css';
+import Todos from './components/todos/Todos';
 
 function App() {
+    const todos = ['Learn React', 'Learn Typescript'];
     return (
         <div className="App">
-
+            <Todos items={todos} />
         </div>
     );
 }
